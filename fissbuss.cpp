6557ca4abe87add27b82ba3777e832193e39b6b4
Naureg <joe.lancas@gmail.com>
2013-12-15 19:31:34 +0000

    Fizzbuzz for perl users!

---
+++ fissbuss.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define y char
+#define Xx if
+#define xX else
+#define Y ==
+#define yY int
+#define yy std::cout
+#define YY <<
+#define xxY std::endl
+
+int main(){y x[5]={70,105,122,122,'\0'};y xx[5]={66,117,x[3],x[3],'\0'};y xY[9]={x[0],105,x[2],122,xx[0],xx[1],xx[2],x[3],'\0'};
+    for(yY i=(x[1]-67);i<=100;i++){Xx((i%((x[0]-67)*5))Y(i^i)){
+            yy YY xY YY xxY;}xX Xx((i%(x[0]-67)Y(i^i))){yy YY x YY xxY;}xX Xx((i%((x[0]-67)+2))Y(i^i)){
+            yy YY xx YY xxY;}xX{
+            yy YY i YY xxY;}}}
